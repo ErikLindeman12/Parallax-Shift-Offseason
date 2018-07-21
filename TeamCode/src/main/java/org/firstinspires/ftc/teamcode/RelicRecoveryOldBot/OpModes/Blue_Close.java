12b3ca0ed2d63e311f8dad2064753cb4a09427e1
Erik Lindeman <LINDE799@umn.edu>
2018-07-21 00:41:25 -0500

    I'm not even sure what I'm doing currently

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryOldBot/OpModes/Blue_Close.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryOldBot/OpModes/Blue_Close.java
@@ -24,7 +24,7 @@ public class Blue_Close extends LinearOpMode {
     public String strafeDistance;
     public String jewelColor;
 
-    /*public CloseableVuforiaLocalizer vuforia;*/
+    public CloseableVuforiaLocalizer vuforia;
 
     private enum currentPhase{
         HIT_JEWEL,
@@ -38,18 +38,54 @@ public class Blue_Close extends LinearOpMode {
         robot = new RobotClass(hardwareMap, gamepad1, telemetry);
 
         robot.initializeServos();
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        CloseableVuforiaLocalizer.Parameters parameters = new CloseableVuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "Aa07QPX/////AAAAGT4IBGftwkAmodz5uX1NKehqWSuZYAizMXyJgDjbMQz+h5mPdKPRRA9id11R2ad9e3w3E6aS1Nep0aXgwwqRtAAmh6tizyQQZRM5qF+foaOh9zbuyAis/ANMODT0X5fAo3J6DqPNlOT9Es04EMKR5rIGhrb91rn3X+ferq2phtQ/PhQGHt44rkhNXSI1OV2GaY4BErnIgSktLZB6bWf49Jd3RtnybC9BfsuOv/2re0pEiGAiF+GyTV5pvuyVVFXFMKaiIR+aDe8qBpKV5z+ZUIWUC+z989ERqh9SKWdfJkOJt6glYFx/fEy3o4g8HwYfVbU+xU1fxufN+M3A2uZZaSSowVbbDDgr9CGxSd6/Dskg";
+        parameters.cameraDirection = CloseableVuforiaLocalizer.CameraDirection.FRONT;
+        this.vuforia = new CloseableVuforiaLocalizer(parameters);
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relictrackable = relicTrackables.get(0);
+        relictrackable.setName("relicVuMark");
 
         currentphase = currentPhase.HIT_JEWEL;
 
         waitForStart();
-        robot.jewelclass.moveDown();
-        jewelColor = robot.jewelclass.scanColor();
-        if(jewelColor == "RED")
-            robot.jewelclass.hitJewel("RIGHT");
-        else
-            robot.jewelclass.hitJewel("LEFT");
-        robot.jewelclass.reset();
-        currentphase = currentPhase.DRIVE_TO_CRYPTO;
+        while(!isStarted()) {
+            relicTrackables.activate();
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relictrackable);
+
+            if (vuMark == RelicRecoveryVuMark.UNKNOWN || vuMark == RelicRecoveryVuMark.LEFT) {
+                telemetry.addData("LEFT", vuMark);
+                telemetry.update();
+            }
+            if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                telemetry.addData("RIGHT", vuMark);
+                telemetry.update();
+            } else {
+                telemetry.addData("CENTER", vuMark);
+                telemetry.update();
+            }
+        }
+        vuforia.close();
+
+        /*switch(currentphase){
+            case HIT_JEWEL:*/
+                robot.jewelclass.moveDown();
+
+                jewelColor = robot.jewelclass.scanColor();
+
+                if(jewelColor == "RED")
+                    robot.jewelclass.hitJewel("RIGHT");
+                else
+                    robot.jewelclass.hitJewel("LEFT");
+
+                robot.jewelclass.reset();
+
+                currentphase = currentPhase.DRIVE_TO_CRYPTO;
+/*
+                break;
+        }*/
+
     }
 
 }
